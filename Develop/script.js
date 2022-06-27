eed06c86d8b61fbd6c358731806596c1755e749c
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-06-26 23:30:16 -0400

    got column to change color according to the time

--- Develop/script.js
+++ Develop/script.js
@@ -2,24 +2,30 @@ var today = moment(); //top day & month
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
 var time = today.format("hh a"); //hour
-var timeRow = $(".hour").find(".hour")
-console.log(timeRow)
+var timeRows = $(".container").find(".hour")
+console.log(timeRows)
 //console.log(Array.isArray(timeRow))
 
 
 function colorCode (){
     //if time is past then color set to gray
-    timeRow.each((element) => {
-        console.log(element.text())
-         if (element < time){
-            element.addClass("past")
-    } else if (element == time){ //if time is present then color set to red
-        element.addClass("present")
+    timeRows.each(function() {
+        let rowMoment = moment($(this).text(),"hh a")
+
+    if (rowMoment.isSame(moment(time, "hh a"))){
+         $(this).parent().parent().find("#timeChange").addClass(["present"])
+
+    } else if (rowMoment.isBefore(moment(time, "hh a"))){ //if time is present then color set to red
+        $(this).parent().parent().find("#timeChange").addClass("past")
+
     } else { //if time is future then color set to green
-        element.addClass("future")
+        $(this).parent().parent().find("#timeChange").addClass("future")
     }
     });
-   
 }
 
 colorCode()
+
+
+
+
